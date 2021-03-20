409ce07ec2cfed45c253ac5e9da7399da98c950d
Jorge E <jorgeesquivelfernandez@gmail.com>
2021-03-19 22:08:29 -0300

    bugs solved

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -45,23 +45,28 @@ export const fetchDishes = () => (dispatch) => {
 
     dispatch(dishesLoading(true));
 
-    return fetch(baseUrl + 'dishes')
-    .then(response => {
+  return fetch(baseUrl + "dishes")
+    .then(
+      (response) => {
         if (response.ok) {
           return response;
         } else {
-          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          var error = new Error(
+            "Error " + response.status + ": " + response.statusText
+          );
           error.response = response;
           throw error;
         }
       },
-      error => {
-            var errmess = new Error(error.message);
-            throw errmess;
-      })
-    .then(response => response.json())
-    .then(dishes => dispatch(addDishes(dishes)))
-    .catch(error => dispatch(dishesFailed(error.message)));
+      (error) => {
+        var errmess = new Error(error.message);
+        throw errmess;
+      }
+    )
+
+    .then((response) => response.json())
+    .then((dishes) => dispatch(addDishes(dishes)))
+    .catch((error) => dispatch(dishesFailed(error.message)));
 }
 
 export const dishesLoading = () => ({
@@ -221,3 +226,4 @@ export const postFeedback = (firstname, lastname, telnum, email, agree, contactT
   )
   .catch(error =>  { console.log('post feedbacks', error.message); alert('Your feedback could not be posted\nError: '+error.message); });
 };
+
